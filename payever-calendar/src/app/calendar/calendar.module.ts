f1832f820ae2685681585ec34c315d462266ca74
Bisma Nawaz <bismnawaz@gmail.com>
2024-08-07 15:04:48 +0500

    udpate

--- payever-calendar/src/app/calendar/calendar.module.ts
+++ payever-calendar/src/app/calendar/calendar.module.ts
@@ -4,8 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core'; // Required for date picker
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CalendarComponent } from './calendar.component';
+
 @NgModule({
   declarations: [CalendarComponent],
   imports: [
@@ -13,8 +17,11 @@ import { CalendarComponent } from './calendar.component';
     FormsModule,
     ReactiveFormsModule,
     MatCardModule,
-    MatDatepickerModule,// Add this line
+    MatDatepickerModule,
+    MatNativeDateModule, // Add this for date support
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule, // Add this for form fields
     DragDropModule
   ],
   exports: [CalendarComponent]
